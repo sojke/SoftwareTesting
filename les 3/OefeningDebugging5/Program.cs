e56abc3e6187dc130ee3676393951f57c9b28c50
SOJKE CALUWE <s096538@ap.be>
2022-10-05 01:06:21 +0200

    oef 5 part 1

--- les 3/OefeningDebugging5/Program.cs
+++ les 3/OefeningDebugging5/Program.cs
@@ -3,16 +3,8 @@
 string w;
 int i, t, ttl;
 List<TimeSheetEntry> ents = new List<TimeSheetEntry>();
-Console.Write("Wat heb je gedaan, voor wie?");
-w = Console.ReadLine();
-Console.Write("Hoe lang heb je gewerkt?");
-t = int.Parse(Console.ReadLine());
 TimeSheetEntry ent = new TimeSheetEntry();
-ent.HoursWorked = t;
-ent.WorkDone = w;
-ents.Add(ent);
-Console.Write("Wil je nog een tijdeenheid ingeven?");
-bool cont = bool.Parse(Console.ReadLine());
+bool cont = false;
 do
 {
     Console.Write("Wat heb je gedaan, voor wie?");
@@ -22,39 +14,48 @@ do
     ent.HoursWorked = t;
     ent.WorkDone = w;
     ents.Add(ent);
-    Console.Write("Wil je nog een tijdeenheid ingeven?");
-    cont = bool.Parse(Console.ReadLine());
+    Console.Write("Wil je nog een tijdeenheid ingeven? (JA/NEE)");
+    if (Console.ReadLine() == "ja")
+    {
+        cont = true;
+    }
+    else
+    {
+        cont = false;
+    }
 } while (cont == true);
 ttl = 0;
 for (i = 0; i < ents.Count; i++)
 {
     if (ents[i].WorkDone.Contains("AP"))
     {
-        ttl += i;
+        ttl += ents[i].HoursWorked;
     }
 }
 Console.WriteLine("Simulatie zenden email naar AP");
-Console.WriteLine("De rekening is €" + ttl * 150 + " voor de gewerkte uren.");
+Console.WriteLine("De rekening is €" + (ttl * 150) + " voor de gewerkte uren.");
+ttl = 0;
 for (i = 0; i < ents.Count; i++)
 {
     if (ents[i].WorkDone.Contains("Microsoft"))
     {
-        ttl += i;
+        ttl += ents[i].HoursWorked;
     }
 }
 Console.WriteLine("Simulatie zenden email naar Microsoft");
-Console.WriteLine("De rekening is €" + ttl * 125 + " voor de gewerkte uren.");
+Console.WriteLine("De rekening is €" + (ttl * 125à + " voor de gewerkte uren.");
+ttl = 0;
 for (i = 0; i < ents.Count; i++)
 {
     ttl += ents[i].HoursWorked;
 }
 if (ttl > 40)
 {
-    Console.WriteLine("Je verdient €" + ttl * 15 + " voor je werk.");
+    Console.WriteLine("Je verdient €" + (ttl * 15) + " voor je werk.");
 }
 else
 {
-    Console.WriteLine("Je verdient €" + ttl * 10 + " voor je werk.");
+    Console.WriteLine("Je verdient €" + (ttl * 10) + " voor je werk.");
 }
 Console.WriteLine();
 Console.Write("Druk op een toets om de applicatie af te sluiten...");
