dc82f5131404d623490644e90a5fb37b00208b55
SOJKE CALUWE <s096538@ap.be>
2022-11-19 00:42:10 +0100

    labo les 8

--- les 8/Opgaven/BowlingGame/BowlingGame.cs
+++ les 8/Opgaven/BowlingGame/BowlingGame.cs
@@ -1,12 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BowlingGameScore
 {
     public class BowlingGame
     {
+        private int[] pins = new int[21];  // het aantal omgeworpen kegels per worp (maximaal 21 worpen laatste frame een spare + 1 of strike + 2;
+        private int currentRoll = 0;       // teller van het aantal worpen die de gebruiker heeft gedaan
+
+        public int Score
+        {
+            get
+            {
+                int score = 0;
+                int rollIndex = 0;
+                for (int frame = 0; frame < 10; frame++) // loop over het aantal worpen
+                {
+
+                    if (isStrike(rollIndex))
+                    {
+                        score += pins[rollIndex] + pins[rollIndex + 1] + pins[rollIndex + 2];
+                        rollIndex++;
+                    }
+                    else if (isSpare(rollIndex))
+                    {
+                        score += getSpareScore(rollIndex);
+                        rollIndex += 2;
+                    }
+                    else
+                    {
+                        score += GetStandardScore(rollIndex);
+                        rollIndex += 2;
+                    }
+                }
+
+                return score;
+            }
+        }
+
+        private bool isStrike(int rollIndex)
+        {
+            return pins[rollIndex] == 10;
+        }
+
+        private int getSpareScore(int rollIndex)
+        {
+            return pins[rollIndex] + pins[rollIndex + 1] + pins[rollIndex + 2];
+        }
+
+        private bool isSpare(int rollIndex)
+        {
+            return pins[rollIndex] + pins[rollIndex + 1] == 10;
+        }
+
+        private int GetStandardScore(int rollIndex)
+        {
+            return pins[rollIndex] + pins[rollIndex + 1];
+        }
+
+        public void Roll(int pinsThisRoll)
+        {
+            pins[currentRoll++] = pinsThisRoll;
+        }
     }
 }
--- les 8/Opgaven/BowlingGameTests/BowlingGameScoreTests.cs
+++ les 8/Opgaven/BowlingGameTests/BowlingGameScoreTests.cs
@@ -6,15 +6,77 @@ namespace BowlingGameScoreTests
     [TestFixture]
     public class BowlingGameScoreTests
     {
+        private BowlingGame game;
+
         [SetUp]
         public void Setup()
         {
+            game = new BowlingGame();
         }
 
         [Test]
-        public void CanCreateGame()
+        public void When_Roll_GlutterGame_Returns_0()
         {
-            var game = new BowlingGame();
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+            var resultaat = game.Score;
+            var verwacht = 0;
+            Assert.AreEqual(resultaat, verwacht);
         }
+
+        [Test]
+        public void When_Roll_AllOnes_Returns_20()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+            var resultaat = game.Score;
+            var verwacht = 20;
+            Assert.AreEqual(resultaat, verwacht);
+        }
+
+        [Test]
+        public void When_Roll_SpareAndThree_Returns_16()
+        {
+            game.Roll(3);
+            game.Roll(7);
+            game.Roll(3); 
+            for (int i = 0; i < 17; i++)
+            {
+                game.Roll(0);
+            }
+            var resultaat = game.Score;
+            var verwacht = 16;
+            Assert.AreEqual(resultaat, verwacht);
+        }
+
+        [Test]
+        public void When_Roll_StrikeAndThreeAndFour_Returns_24()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(4);
+            for (int i = 0; i < 16; i++)
+            {
+                game.Roll(0);
+            }
+            var resultaat = game.Score;
+            var verwacht = 24;
+            Assert.AreEqual(resultaat, verwacht);
+        }
+        [Test]
+        public void When_Roll_PerfectGame_Returns_300()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Roll(10);
+            }
+            var resultaat = game.Score;
+            var verwacht = 300;
+            Assert.AreEqual(resultaat, verwacht);
+        }   
     }
 }
